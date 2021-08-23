2cef5d66ceedfd3dc87e1ab85009046bace34dbf
romani4615 <romani4615@gmail.com>
2021-08-23 11:12:37 -0400

    working Register

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react'
 import Nav from './components/Nav';
 import {Switch, Route} from'react-router-dom';
-import Home from './views/Log';
-import About from'./views/Log';
+import Home from './views/Home';
+
 import SignUp from './views/SignUp';
 import Products from './views/Products';
 import Cart from './views/Cart';
-
-
-
-
-
-
+import Login from './views/Login';
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isLoggedIn: false
+    }
+  }
+// handleAdd = (itemId) => {
+
+// }
+
+  handleLogIn = (e) => {  // onsubmit in login runs this function with this.props.handlelogin
+    e.preventDefault(); //stops from re running
+    console.log(e);
+    // Grab data from form
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+  
+    let myHeaders = new Headers();
+    const credentials = (`${username}:${password}`);
+    myHeaders.append('Authorization', 'Basic ' + credentials);
+  
+    fetch('https://cart-api-66.herokuapp.com/tokens', {
+      method: 'POST',
+      headers: myHeaders
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      this.setState({
+        isLoggedIn: true  //dependent on if user is logged in
+      })
+      localStorage.setItem('token', data.token)  //hover set item
+    })
+  }
+
+
+  handleLogOut = () => {
+    localStorage.removeItem('token');
+    this.setState({
+      isLoggedIn: false
+    })
+  }
   render() {
     return (
       
       
       <div>
-        <Nav/>
+        <Nav isLoggedIn={this.state.isLoggedIn} logOut={this.handleLogOut}/>
         <Switch>
+        <Route exact path="/login">
+          <Login handleLogIn={this.handleLogIn}/> 
+        </Route>
+        
           <Route exact path ='/' render = { ()=> <Home/>}/>
-          <Route exact path ='/About' render ={ ()=> <About/>}/>
           <Route exact path = '/SignUp' render ={ ()=> <SignUp/>}/>
           <Route exact path = '/Products' render ={ ()=> <Products/>}/>
           <Route exact path = '/Cart' render ={ ()=> <Cart/>}/>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,39 +1,45 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default class Nav extends Component {
-    render() {
-        return (
-          
-          <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">Welcome!</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <Link class="nav-link" to="/">Sign In<span class="sr-only"></span></Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/SignUp">Sign Up</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/Products">Products </Link>
-              </li>
-              
+  render() {
+    return (
 
-              <li class="nav-item">
-                <Link class="nav-link" to="/Cart">Cart </Link>
+      <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <h5 className="navbar-brand" href="#">Welcome!</h5>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/SignUp">Sign Up</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/Products">Products </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/Cart">Cart </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto">
+            {this.props.isLoggedIn ? (
+              <li className="nav-item">
+                <button className="btn btn-outline-success nav-link" onClick={this.props.logOut} to="/">Logout</button>
               </li>
-            </ul>
-            
-          </div>
-          <div className="auth-wrapper">
-        <div className="auth-inner">
+            ) : ( //if login in true show logout, of false, show login
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li>
+            )
+            }
+          </ul>
+        </div>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
           </div>
         </div>
-        </nav>
-        )
-        }
-      }
+      </nav>
+    )
+  }
+}
